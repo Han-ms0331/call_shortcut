996a4ef23a24ba0aee5ae54245ff21e3843f5249
한명수 <hmsoo@hanmyeongsuui-MacBookPro.local>
2021-05-05 19:02:22 +0900

    add input system

--- App.js
+++ App.js
@@ -1,14 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useState} from 'react';
+import { StyleSheet, Text, View ,TouchableOpacity, TextInput} from 'react-native';
+let list = [];
+let count = 0;
+let asdasd = {phoneNum : ''};
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [IsAdd, setIsAdd] = useState(false);
+  const [phoneNum, setPhoneNum] = useState('');
+  const [listEmpty, setListEmpty] = useState(true);
+  const addPress = () => {
+    setIsAdd(true);
+  };
+  const savePress = () => {
+    asdasd = {phoneNumber: `${phoneNum}`};
+    list[count] = asdasd;
+    setIsAdd(false);
+  }
+  const InputPhone = (num) => {
+    setPhoneNum(num);
+  }
+     
+    if(!IsAdd){
+      if(list.length === 0){
+        return (
+          <View style={styles.container}>
+              {console.log(list[0])}
+              <View>
+                <Text>
+                  새 번호를 추가해 보세요!
+                </Text>
+              </View>
+              <TouchableOpacity style={styles.add_btn} onPress={addPress}>
+                <Text>전화번호 추가</Text>
+              </TouchableOpacity>
+            <StatusBar style="auto" />
+          </View>
+        );
+      } else{
+        return (
+          <View style={styles.container}>
+              {console.log(list[0])}
+              <View>
+                <Text>
+                  {
+                  list[0].phoneNumber
+                  }
+                </Text>
+              </View>
+              <TouchableOpacity style={styles.add_btn} onPress={addPress}>
+                <Text>전화번호 추가</Text>
+              </TouchableOpacity>
+            <StatusBar style="auto" />
+          </View>
+        );
+      }
+     
+    } else{
+      return(
+        <View style={styles.container}>
+            <TextInput
+            onChangeText={InputPhone}
+            placeholder="아무거나 입력해주세요."
+            />
+            <TouchableOpacity style={styles.add_btn} onPress={savePress}>
+              <Text>저장</Text>
+            </TouchableOpacity>
+          <StatusBar style="auto" />
+        </View>
+      );
+    }
+  
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +79,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  add_btn: {
+    backgroundColor:'gray'
+  }
 });
